552175ab5cc5fef49dcabdadfb7bb7082543af9b
Jacob Odetunde <jacodetunde@gmail.com>
2023-08-22 20:20:09 -0500

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
-@EnableWebMvc
 public class DemoApplication {
 
     public static void main(String[] args) {
@@ -15,10 +15,15 @@ public class DemoApplication {
     }
 
     @Bean
-    public InternalResourceViewResolver viewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".jsp");
-        return resolver;
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void configureViewResolvers(ViewResolverRegistry registry) {
+                InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+                resolver.setPrefix("/WEB-INF/views/");
+                resolver.setSuffix(".jsp");
+                registry.viewResolver(resolver);
+            }
+        };
     }
 }
